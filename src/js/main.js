f748eea5adeb53dabcb4b4a124d820a7bd31295c
David Milivojev <davidmilivojev@gmail.com>
2022-03-19 21:58:13 +0100

    Updated layout

--- src/js/main.js
+++ src/js/main.js
@@ -605,118 +605,118 @@ function joinPie5() {
     }
 }
 
-function animatedBar() {
-    var svg = d3.select("#bar1"),
-        width = svg.attr("width") - 20,
-        height = svg.attr("height") - 120;
+// function animatedBar() {
+//     var svg = d3.select("#bar1"),
+//         width = svg.attr("width") - 20,
+//         height = svg.attr("height") - 120;
 
-    var x = d3.scaleBand().range([0, width]).padding(0.4),
-        y = d3.scaleLinear().range([height, 0]);
-    var color = d3.scaleOrdinal().range(themeColors);
+//     var x = d3.scaleBand().range([0, width]).padding(0.4),
+//         y = d3.scaleLinear().range([height, 0]);
+//     var color = d3.scaleOrdinal().range(themeColors);
 
-    var g = svg.append("g")
-            .attr("transform", "translate(" + 0 + "," + 80 + ")");
-    // symbolTriangle
-    var sym = d3.symbol().type(d3.symbolTriangle).size(200);
+//     var g = svg.append("g")
+//             .attr("transform", "translate(" + 0 + "," + 80 + ")");
+//     // symbolTriangle
+//     var sym = d3.symbol().type(d3.symbolTriangle).size(200);
 
-    d3.csv("bar.csv", function(error, data) {
-        if (error) {
-            throw error;
-        }
+//     d3.csv("bar.csv", function(error, data) {
+//         if (error) {
+//             throw error;
+//         }
 
-        x.domain(data.map(function(d) { return d.nazivSektora; }));
-        y.domain([0, d3.max(data, function(d) { return d.budzet; })]);
+//         x.domain(data.map(function(d) { return d.nazivSektora; }));
+//         y.domain([0, d3.max(data, function(d) { return d.budzet; })]);
 
-        g.append("g")
-         .attr("transform", "translate(0," + height + ")")
-         .call(d3.axisBottom(x).tickFormat(function(d){
-            return d;
-        }))
+//         g.append("g")
+//          .attr("transform", "translate(0," + height + ")")
+//          .call(d3.axisBottom(x).tickFormat(function(d){
+//             return d;
+//         }))
 
-        g.selectAll(".bar")
-         .data(data)
-         .enter().append("rect")
-         .attr("class", "bar")
-         .on("mouseover", onMouseOver) //Add listener for the mouseover event
-         .on("mouseout", onMouseOut)   //Add listener for the mouseout event
-         .attr("x", function(d) { return x(d.nazivSektora); })
-         .attr("y", function(d) { return y(d.budzet); })
-         .attr("width", x.bandwidth())
-         .transition()
-         .ease(d3.easeLinear)
-         .duration(400)
-         .delay(function (d, i) {
-             return i * 50;
-         })
-         .style("fill", function(d, i) {
-            return color(i);
-         })
-         .attr("height", function(d) { return height - y(d.budzet); });
-    });
+//         g.selectAll(".bar")
+//          .data(data)
+//          .enter().append("rect")
+//          .attr("class", "bar")
+//          .on("mouseover", onMouseOver) //Add listener for the mouseover event
+//          .on("mouseout", onMouseOut)   //Add listener for the mouseout event
+//          .attr("x", function(d) { return x(d.nazivSektora); })
+//          .attr("y", function(d) { return y(d.budzet); })
+//          .attr("width", x.bandwidth())
+//          .transition()
+//          .ease(d3.easeLinear)
+//          .duration(400)
+//          .delay(function (d, i) {
+//              return i * 50;
+//          })
+//          .style("fill", function(d, i) {
+//             return color(i);
+//          })
+//          .attr("height", function(d) { return height - y(d.budzet); });
+//     });
 
-    //mouseover event handler function
-    function onMouseOver(d, i) {
-        d3.select(this).attr('class', 'highlight');
-        d3.select(this)
-          .transition()     // adds animation
-          .duration(400)
-          .attr('width', x.bandwidth() + 5)
-          .attr("y", function(d) { return y(d.budzet) - 10; })
-          .attr("height", function(d) { return height - y(d.budzet) + 10; });
+//     //mouseover event handler function
+//     function onMouseOver(d, i) {
+//         d3.select(this).attr('class', 'highlight');
+//         d3.select(this)
+//           .transition()     // adds animation
+//           .duration(400)
+//           .attr('width', x.bandwidth() + 5)
+//           .attr("y", function(d) { return y(d.budzet) - 10; })
+//           .attr("height", function(d) { return height - y(d.budzet) + 10; });
 
-          g.append("rect")
-          .attr('class', 'val')
-          .attr('x', function() {
-              var xWidth = (x(d.nazivSektora) - 55);
-              return xWidth;
-          })
-          .attr('y', function() {
-              return y(d.budzet) - 65;
-          })
-          .attr('rx', 5)
-          .attr('width', 140)
-          .attr("height", 40)
-          .attr('fill', '#E9E9E9')
+//           g.append("rect")
+//           .attr('class', 'val')
+//           .attr('x', function() {
+//               var xWidth = (x(d.nazivSektora) - 55);
+//               return xWidth;
+//           })
+//           .attr('y', function() {
+//               return y(d.budzet) - 65;
+//           })
+//           .attr('rx', 5)
+//           .attr('width', 130)
+//           .attr("height", 40)
+//           .attr('fill', '#E9E9E9')
 
-          g.select('g')
-          .append("path")
-          .attr("d", sym)
-          .attr("fill", "#E9E9E9")
-          .attr('class', 'val')
-          .attr('transform', function() {
-              var xWidth = x(d.nazivSektora) + 16;
-              var xHeight = (height - y(d.budzet) + 20)*(-1);
-              return "translate(" + xWidth + "," + xHeight + ") rotate(180)";
-          })
+//           g.select('g')
+//           .append("path")
+//           .attr("d", sym)
+//           .attr("fill", "#000")
+//           .attr('class', 'val')
+//           .attr('transform', function() {
+//               var xWidth = x(d.nazivSektora) + 16;
+//               var xHeight = (height - y(d.budzet) + 20)*(-1);
+//               return "translate(" + xWidth + "," + xHeight + ") rotate(180)";
+//           })
 
-          g.append("text")
-         .attr('class', 'val')
-         .attr('x', function() {
-             return x(d.nazivSektora) - 30;
-         })
-         .attr('y', function() {
-             return y(d.budzet) - 40;
-         })
-         .text(function() {
-             return [ d.budzet + " milijardi din."];
-         });
-    }
+//           g.append("text")
+//          .attr('class', 'val')
+//          .attr('x', function() {
+//              return x(d.nazivSektora) - 30;
+//          })
+//          .attr('y', function() {
+//              return y(d.budzet) - 40;
+//          })
+//          .text(function() {
+//              return [ d.budzet + " milijardi din."];
+//          });
+//     }
 
-    //mouseout event handler function
-    function onMouseOut(d, i) {
-        // use the text label class to remove label on mouseout
-        d3.select(this).attr('class', 'bar');
-        d3.select(this)
-          .transition()     // adds animation
-          .duration(400)
-          .attr('width', x.bandwidth())
-          .attr("y", function(d) { return y(d.budzet); })
-          .attr("height", function(d) { return height - y(d.budzet); });
+//     //mouseout event handler function
+//     function onMouseOut(d, i) {
+//         // use the text label class to remove label on mouseout
+//         d3.select(this).attr('class', 'bar');
+//         d3.select(this)
+//           .transition()     // adds animation
+//           .duration(400)
+//           .attr('width', x.bandwidth())
+//           .attr("y", function(d) { return y(d.budzet); })
+//           .attr("height", function(d) { return height - y(d.budzet); });
 
-        d3.selectAll('.val')
-          .remove()
-    }
-}
+//         d3.selectAll('.val')
+//           .remove()
+//     }
+// }
 
 function horizontalBar() {
     d3.selectAll(".hbar .radio").on("change", updateData);
@@ -728,7 +728,7 @@ function horizontalBar() {
 
         var svg = d3.select("#graphic"),
             width = svg.attr("width") - 20,
-            height = svg.attr("height") - 60;
+            height = svg.attr("height") - 80;
 
             svg.selectAll("g").remove();
             svg.selectAll("rect").remove();
@@ -795,6 +795,7 @@ function horizontalBar() {
 
         // add the x Axis
         svg.append("g")
+            .attr("class", "xtick")
             .attr("transform", "translate(0," + height + ")")
             .call(d3.axisBottom(x));
 
@@ -811,7 +812,7 @@ function animatedBar() {
         width = svg.attr("width"),
         height = svg.attr("height") - 120;
 
-    var x = d3.scaleBand().range([0, width]).padding(0.5),
+    var x = d3.scaleBand().range([0, width]).padding(0.4),
         y = d3.scaleLinear().range([height, 0]);
     var color = d3.scaleOrdinal().range(themeColors);
 
@@ -1561,10 +1562,10 @@ function getMap() {
         .range(d3.range(6).map(function(i) { return "q" + i + "-9"; }));
 
 	var projection = d3.geoAlbers()
-        .center([0, 41.8])
-        .rotate([-25.4, 0])
+        .center([0, 42])
+        .rotate([-24.4, 0])
         .parallels([40, 50])
-        .scale(4400)
+        .scale(4700)
         .translate([width / 2, height / 2]);
 
 	var path = d3.geoPath()
@@ -1576,14 +1577,14 @@ function getMap() {
 
 	svg.append("text")
         .text("")
-        .attr("dx","10em")
+        .attr("dx","14em")
         .attr("dy","2em")
         .attr("font-size", 14)
         .classed("stat", true);
 
     svg.append("text")
         .text("")
-        .attr("dx","10em")
+        .attr("dx","14em")
         .attr("dy","3.5em")
         .attr("font-size", 14)
         .attr("font-weight","bold")
@@ -1625,8 +1626,8 @@ getData();
 joinPie();
 // joinPie2();
 // joinPie3();
-joinPie4();
-joinPie5();
+// joinPie4();
+// joinPie5();
 animatedBar();
 horizontalBar()
 // animatedBar2();
